80af987eb1647d38c50cc41c79f98f6a61f5c631
SaitoTsubasa <k021g1226@g.neec.ac.jp>
2022-06-06 11:49:50 +0900

    Gitリポジトリ提出(シューティング)

---
+++ Shooting/Assets/Bullet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        pos.z += 0.05f;
+
+        transform.position = new Vector3(pos.x, pos.y, pos.z);
+
+        if (pos.z >= 20) 
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
